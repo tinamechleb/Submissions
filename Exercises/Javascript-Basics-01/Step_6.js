6a99329944d9091324c11a29ab60656489b927c0
tinamechleb <tinamechleb@gmail.com>
2019-12-16 16:38:51 +0200

    Step_6

---
+++ Exercises/Javascript-Basics-01/Step_6.js
@@ -0,0 +1,9 @@
+var fn = prompt("Enter first number: ");
+
+var sn = prompt("Enter second number: ");
+
+var x = fn / sn;
+
+var mod = fn % sn;
+
+alert("The remainder of " + fn + " and " + sn + " is " + mod);
