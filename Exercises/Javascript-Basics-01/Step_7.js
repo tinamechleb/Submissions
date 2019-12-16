3bffebcfd76534b3de87ad4d6ad0b314e4b18454
tinamechleb <tinamechleb@gmail.com>
2019-12-16 16:49:10 +0200

    Step_7

---
+++ Exercises/Javascript-Basics-01/Step_7.js
@@ -0,0 +1,8 @@
+var ss = prompt("Enter your shoe size: ");
+var bb = prompt("Enter your birth year: ");
+
+function step7() {
+  var x = (ss * 2 + 5) * 50 - bb + 1766;
+  return x;
+}
+alert(step7());
