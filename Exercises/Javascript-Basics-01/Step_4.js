705eae6c575c7901c0a9db087412a9af2e1dce78
tinamechleb <tinamechleb@gmail.com>
2019-12-16 15:17:47 +0200

    Step_4

---
+++ Exercises/Javascript-Basics-01/Step_4.js
@@ -0,0 +1,5 @@
+var name = prompt("Enter your name: ");
+var surname = prompt("Enter your surname: ");
+var city = prompt("Enter your city: ");
+
+alert("Name: " + name + "\nSurname: " + surname + "\nCity: " + city);
