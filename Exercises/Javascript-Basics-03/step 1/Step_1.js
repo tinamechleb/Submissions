09e53d437cc2fdfd562fe5135f76d42979ffe5f6
tinamechleb <tinamechleb@gmail.com>
2019-12-17 14:28:10 +0200

    Step_1_Basics_03

---
+++ Exercises/Javascript-Basics-03/step 1/Step_1.js
@@ -0,0 +1,6 @@
+document.querySelector("#image1").addEventListener("mouseover", () => {
+  document.getElementById("image1").style.border = "3px solid red";
+});
+document.querySelector("#image1").addEventListener("mouseout", () => {
+  document.getElementById("image1").style.border = "none";
+});
