2402fef31efeb11334e8e614003339e955b3e0f8
tinamechleb <tinamechleb@gmail.com>
2019-12-17 20:16:31 +0200

    Step_3_Basics_03

---
+++ Exercises/Javascript-Basics-03/step 3/Step_3.js
@@ -0,0 +1,9 @@
+document.querySelector(".green").addEventListener("click", () => {
+  document.querySelector("p").style.color = "green";
+});
+document.querySelector(".red").addEventListener("click", () => {
+  document.querySelector("p").style.color = "red";
+});
+document.querySelector(".blue").addEventListener("click", () => {
+  document.querySelector("p").style.color = "blue";
+});
