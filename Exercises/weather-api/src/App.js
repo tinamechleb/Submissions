847394fbe697d04db05cc2554ec7a175cd86f623
tinamechleb <tinamechleb@gmail.com>
2020-01-13 15:05:49 +0200

    added weather icons

--- Exercises/weather-api/src/App.js
+++ Exercises/weather-api/src/App.js
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
+import clear from "../src/img/weather-icons/clear.svg";
+import cloudy from "../src/img/weather-icons/cloudy.svg";
+import drizzle from "../src/img/weather-icons/drizzle.svg";
+import fog from "../src/img/weather-icons/fog.svg";
+import mostlycloudy from "../src/img/weather-icons/mostlycloudy.svg";
+import partlycloudy from "../src/img/weather-icons/partlycloudy.svg";
+import rain from "../src/img/weather-icons/rain.svg";
+import snow from "../src/img/weather-icons/snow.svg";
+import storm from "../src/img/weather-icons/storm.svg";
+import unknown from "../src/img/weather-icons/unknown.svg";
 
 class App extends Component {
   constructor(props) {
@@ -18,7 +28,7 @@ class App extends Component {
         </header>
         <main className="app__main">
           <div>
-            <img src="https://placekitten.com/200/138"></img>
+            <img src={mostlycloudy} alt="clear-icon"></img>
             <p>overcast clouds</p>
             <h4>
               Temperature <span>10°C to 11°C</span>
@@ -30,37 +40,37 @@ class App extends Component {
           <div id="gridss">
             <div>
               <h5>03:00</h5>
-              <img src="https://placekitten.com/96/96"></img>
+              <img src={mostlycloudy}></img>
               <h5>8°C</h5>
             </div>
             <div>
               <h5>06:00</h5>
-              <img src="https://placekitten.com/96/96"></img>
+              <img src={mostlycloudy}></img>
               <h5>9°C</h5>
             </div>
             <div>
               <h5>09:00</h5>
-              <img src="https://placekitten.com/96/96"></img>
+              <img src={clear}></img>
               <h5>14°C</h5>
             </div>
             <div>
               <h5>12:00</h5>
-              <img src="https://placekitten.com/96/96"></img>
+              <img src={clear}></img>
               <h5>17°C</h5>
             </div>
             <div>
               <h5>15:00</h5>
-              <img src="https://placekitten.com/96/96"></img>
+              <img src={clear}></img>
               <h5>18°C</h5>
             </div>
             <div>
               <h5>18:00</h5>
-              <img src="https://placekitten.com/96/96"></img>
+              <img src={clear}></img>
               <h5>16°C</h5>
             </div>
             <div>
               <h5>21:00</h5>
-              <img src="https://placekitten.com/96/96"></img>
+              <img src={mostlycloudy}></img>
               <h5>13°C</h5>
             </div>
           </div>
