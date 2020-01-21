357a2ea74d197953afe807e05452206d7b5ce229
tinamechleb <tinamechleb@gmail.com>
2020-01-21 15:33:28 +0200

    fixed answers.sh in SQL

---
+++ Exercises/SQLite Basic/answers.sh
@@ -0,0 +1,21 @@
+"SELECT Name FROM students"
+"SELECT * FROM students WHERE Age > 30"
+"SELECT Name FROM students WHERE Age = 30 AND Gender = 'F'"
+"SELECT Points FROM students WHERE Name = 'Alex'"
+"INSERT INTO students (name,Age,Gender,Points) VALUES ('Tina',18,'F',100)"
+"UPDATE students SET Points = Points + 100 WHERE Name = 'Basma'"
+"UPDATE students SET Points = Points - 20 WHERE Name = 'Alex'"
+
+"INSERT INTO graduates (ID, Name, Age, Gender, Points) SELECT ID, Name, Age, Gender, Points FROM students WHERE Name = 'Layal'"
+"UPDATE graduates SET Graduation = '08/09/2018' WHERE Name = 'Layal'"
+"DELETE FROM students WHERE Name = 'Layal'"
+
+"SELECT employees.Name, employees.Company, companies.Date FROM employees INNER JOIN companies ON employees.Company=companies.Name"
+"SELECT employees.Name FROM employees INNER JOIN companies ON employees.Company=companies.Name WHERE companies.Date<2000"
+"SELECT companies.Name FROM employees INNER JOIN companies ON employees.Company=companies.Name WHERE employees.Role='Graphic Designer'"
+
+"SELECT Name FROM students WHERE Points=(SELECT max(Points) FROM students)"
+"SELECT avg(Points) FROM students"
+"SELECT count() FROM students WHERE Points = 500"
+"SELECT Name FROM students WHERE Name like '%s%'"
+"SELECT Name FROM students ORDER BY Points DESC"
