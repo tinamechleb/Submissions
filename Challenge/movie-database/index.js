f2d89c550b273446df546ba6d1f3d2eb7d196173
tinamechleb <tinamechleb@gmail.com>
2020-01-22 12:51:33 +0200

    step 3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -5,4 +5,12 @@ app.get("/", function(req, res) {
   res.send("ok");
 });
 
+app.get("/test", function(req, res) {
+  res.send({ status: 200, message: "ok" });
+});
+
+app.get("/time", function(req, res) {
+  res.send({ status: 200, message: "12:50" });
+});
+
 app.listen(3000, function() {});
