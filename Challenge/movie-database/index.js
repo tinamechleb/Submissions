b30c12bc8c8066a42c8d20b3c4dca6eeb45988d5
tinamechleb <tinamechleb@gmail.com>
2020-01-23 12:16:44 +0200

    step 5

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -36,4 +36,27 @@ app.get("/search*", function(req, res) {
   }
 });
 
+app.get("/movies/add", function(req, res) {
+  res.send("add");
+});
+
+app.get("/movies/get", function(req, res) {
+  res.send({ status: 200, data: movies });
+});
+
+app.get("/movies/edit", function(req, res) {
+  res.send("edit");
+});
+
+app.get("/movies/delete", function(req, res) {
+  res.send("delete");
+});
+
 app.listen(3000);
+
+const movies = [
+  { title: "Jaws", year: 1975, rating: 8 },
+  { title: "Avatar", year: 2009, rating: 7.8 },
+  { title: "Brazil", year: 1985, rating: 8 },
+  { title: "الإرهاب والكباب‎", year: 1992, rating: 6.2 }
+];
