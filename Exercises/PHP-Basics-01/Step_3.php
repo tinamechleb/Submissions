ef0fd61c226d96d215a62ac4f55f901ffdaeb70e
tinamechleb <tinamechleb@gmail.com>
2020-03-04 12:07:59 +0200

    Step 3

---
+++ Exercises/PHP-Basics-01/Step_3.php
@@ -0,0 +1,12 @@
+<?php
+    $name = "Tina";
+    $email = "tinamechleb@gmail.com";
+
+    echo "My name is ${name} and my email is ${email}\n";
+
+    $height = 6;
+    $width = 5;
+    $area = $height*$width;
+
+    echo $area;
+?>
