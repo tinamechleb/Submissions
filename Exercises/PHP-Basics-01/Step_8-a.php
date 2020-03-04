9edb350c805a782ad819b4c508f15f3e4660910b
tinamechleb <tinamechleb@gmail.com>
2020-03-04 13:03:41 +0200

    Step 8 a

---
+++ Exercises/PHP-Basics-01/Step_8-a.php
@@ -0,0 +1,5 @@
+<?php 
+    $groceries = ["Eggs", "Milk", "Cheese", "Water","Pack", "Tissues", "Watermelon"];
+
+    echo "Hello Sir, do you have $groceries[0], $groceries[1], and $groceries[2]? Also if you sell fruits can I find a $groceries[6]?\n"
+?>
