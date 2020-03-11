d4d2c50d61579a41eabb8e9494000655e9461a3b
tinamechleb <tinamechleb@gmail.com>
2020-03-11 20:50:52 +0200

    Step 9

--- Exercises/PHP-Basics-02/Step_9.php
+++ Exercises/PHP-Basics-02/Step_9.php
@@ -1,15 +1,8 @@
 <?php
-     function sqrtOf($num) {
-        if($num === 0) {
-            return "hu2\n";
-        }
-        else {
-            if(($num & ($num - 1)) == 0) {
-                return "${num} is power of 2\n";
-            }
-            else {
-                return "hu2\n";
-            }
-        }
+    function sqrtOf($num) {
+        $squareayre = sqrt($num);
+        echo $squareayre;
     }
+
+    sqrtOf(16);
 ?>
