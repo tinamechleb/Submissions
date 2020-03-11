5faaa9db22d67a41cf40284738742468574a91fb
tinamechleb <tinamechleb@gmail.com>
2020-03-11 20:39:33 +0200

    php basics 03 DONE

---
+++ Exercises/PHP-Basics-03/4_PHPFunctions_1.php
@@ -0,0 +1,11 @@
+<?php
+    $personal_details=array("name" => "Rajesh Rao", "occupation" => "Engineer", "age" => 39, "country" => "India");
+    
+    function AnFunction($personal_details) {
+        foreach ( $personal_details as $key => $value ){
+            echo "$key=$value\n";
+        }
+    }
+
+    AnFunction($personal_details)
+?>
