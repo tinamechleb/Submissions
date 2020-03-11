4b44b921ab1dda135f75eff69467e2aa41415cbc
tinamechleb <tinamechleb@gmail.com>
2020-03-11 21:16:38 +0200

    Step 10

--- Exercises/PHP-Basics-02/Step_10.php
+++ Exercises/PHP-Basics-02/Step_10.php
@@ -1,3 +1,17 @@
 <?php
-    
+    function check_palindrome($string) {
+        function ed7ashni($string) {
+            $string = str_replace(' ', '', $string);
+            return $string;
+        }
+
+        if(ed7ashni($string) == ed7ashni(strrev($string))) {
+            return "c'est un palindrome";
+        }
+        else {
+            return "hu2";
+        }
+    }
+
+    echo check_palindrome("nurses run");
 ?>
